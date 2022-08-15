182db37d2753c73c8ff53054ada1a3b93c98adac
dansheikh <dan.sheikh@yahoo.com>
2022-08-14 21:47:26 -0400

    Added fish nix config.

--- nixpkgs/.config/nixpkgs/modules/home.nix
+++ nixpkgs/.config/nixpkgs/modules/home.nix
@@ -36,4 +36,31 @@
     programs.home-manager.enable = true;
     programs.direnv.enable = true;
     programs.direnv.nix-direnv.enable = true;
+    programs.fish = {
+        enable = true;
+        interactiveShellInit = "
+            set EDITOR nvim
+            set -x LANG en_US.UTF-8
+            set -x LC_TYPE en_US.UTF-8
+
+            starship init fish | source
+        ";
+        shellAbbrs = {
+            .. = "cd ..";
+            ... = "cd ../..";
+            giad = "git add";            
+            gich = "git checkout";
+            gicl = "git clone";
+            gico = "git commit";            
+            gidi = "git diff";
+            gihi = "git history";           
+            gist = "git status";
+        };
+        shellAliases = {
+            cp = "cp -iv"
+            mkdir = "mkdir -pv"
+            mv = "mv -iv"
+            rm = "rm -iv"
+        };
+    };
 }
